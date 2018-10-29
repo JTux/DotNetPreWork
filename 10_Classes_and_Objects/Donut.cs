d5b82a44379071c6c7559d2c9d5e3b42d3f8b2e5
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:50:06 -0400

    Properties

--- 10_Classes_and_Objects/Donut.cs
+++ 10_Classes_and_Objects/Donut.cs
@@ -8,5 +8,9 @@ namespace _10_Classes_and_Objects
 {
     class Donut
     {
+        public string Type { get; set; }
+        public string Filling { get; set; }
+        public decimal Price { get; set; }
+        public bool IsSpecial { get; set; }
     }
 }
--- 10_Classes_and_Objects/Program.cs
+++ 10_Classes_and_Objects/Program.cs
@@ -13,6 +13,24 @@ namespace _10_Classes_and_Objects
             Donut dougDonut = new Donut();
             Donut chrisDonut = new Donut();
             Donut nickDonut = new Donut();
+
+            dougDonut.Filling = "Cherry";
+            dougDonut.Price = 3m;
+            dougDonut.Type = "Normal";
+            dougDonut.IsSpecial = true;
+
+            chrisDonut.Filling = "none";
+            chrisDonut.Price = 2m;
+            chrisDonut.Type = "Small";
+            chrisDonut.IsSpecial = false;
+
+            nickDonut.Filling = "Chocolate";
+            nickDonut.Price = 3m;
+            nickDonut.Type = "Normal";
+            nickDonut.IsSpecial = true;
+
+            Console.WriteLine(nickDonut.Filling);
+            Console.ReadLine();
         }
     }
 }
