9e11f9fe8c3374cfbefbcd6de6d2a6cd921ea353
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:24:14 -0400

    Strings

---
+++ 05_Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            string wholeName = "Joshua Tucker";
+
+            string first = "The cars we sell are";
+            string second = "BMW, Lexus, and Mercedes.";
+
+            Console.WriteLine(first + " " + second);
+            Console.WriteLine("{0} {1}", first, second);
+            Console.WriteLine($"{first} {second}");
+
+            Console.ReadLine();
+        }
+    }
+}
