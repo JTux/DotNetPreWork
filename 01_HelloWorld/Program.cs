9c6e542913c023b7fecf0e692fc64f684c3a9935
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:05:30 -0400

    HelloWorld

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Hello friend. How are you this fine October day?");
+            Console.WriteLine("I'm well, yourself?");
+            Console.WriteLine("I am fantastic! I'm learning how to code!");
+            Console.WriteLine("Wow! That's great!");
+            Console.ReadLine();
+        }
+    }
+}
--- 01_HelloWorld/Properties/AssemblyInfo.cs
+++ 01_HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("01_HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyProduct("01_HelloWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5ab8ee3-c2db-42a9-8a2e-eaba87641005")]
+[assembly: Guid("e4ea78f3-f100-4805-8ab9-f5beef8add2e")]
 
 // Version information for an assembly consists of the following four values:
 //
