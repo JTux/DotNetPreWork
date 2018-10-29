f578d5a360d59e0290f33437e9e229da94d51495
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:32:45 -0400

    Conditionals Switch

---
+++ 07_Conditionals_Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Conditionals_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string inputName = Console.ReadLine();
+
+            switch (inputName.ToLower())
+            {
+                case "fred":
+                    Console.WriteLine("Hey Fred let's go golfing.");
+                    break;
+                case "karl":
+                    Console.Write("Let's hang.");
+                    break;
+                case "john":
+                    Console.WriteLine("Sorry, I am busy right now.");
+                    break;
+                default:
+                    Console.WriteLine("Hey " + inputName + " can I call you back in a minute?");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
