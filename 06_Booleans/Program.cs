28975f0038f516f32d125d543c458d5842b73e7c
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:27:38 -0400

    Booleans

---
+++ 06_Booleans/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSad = false;
+            bool isHappy = true;
+
+            double value = 101d / 2d;
+
+            if (value == 50)
+            {
+                Console.WriteLine(isHappy);
+            }
+            else if (value != 50)
+            {
+                Console.WriteLine(isSad);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
