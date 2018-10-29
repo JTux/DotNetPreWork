be43c9d9d9e1305483281d115276028146fa5f34
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:08:17 -0400

    Variables

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Joshua";
+            Console.WriteLine(firstName);
+
+            string lastName = "Tucker";
+            Console.WriteLine(lastName);
+
+            int birthYear = 1995;
+
+            Console.ReadLine();
+        }
+    }
+}
