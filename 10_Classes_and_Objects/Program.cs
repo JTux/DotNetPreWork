76a73bc04f918919da2043736bffa9eabbd8795a
Joshua Tucker <thejltucker@gmail.com>
2018-10-28 22:47:01 -0400

    Classes and Objects

---
+++ 10_Classes_and_Objects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Classes_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Donut dougDonut = new Donut();
+            Donut chrisDonut = new Donut();
+            Donut nickDonut = new Donut();
+        }
+    }
+}
